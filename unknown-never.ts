e0b912df04cc2b792c9eae92c0102b16e7b4dbfa
Tyler Boskay <tylerboskay@gmail.com>
2021-11-25 23:20:03 -0600

    Fixed File names & Added TSWatch Script for debug

---
+++ unknown-never.ts
@@ -0,0 +1,13 @@
+//Unknown Type:
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "Max";
+
+//Never Type:
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+}
+
+const result = generateError("An error occured!", 500);
